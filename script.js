eb544203b4b7268418cbcfea79c2b4af5e177b14
EAWC system <162746043+eawcsystem@users.noreply.github.com>
2025-01-25 18:04:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const users = [
     { username: 'Gast', pin: 'Gast2025', rank: 'Gast' },
     { username: 'eawc-marcel', pin: 'Marcel2009', rank: 'Inhaber' },   
     { username: 'eawc-marc', pin: 'Marc2009', rank: 'stv.Inhaber' },
-    { username: 'support-user', pin: 'Support2025', rank: 'Support' } // Example Support user
+    { username: 'support-user', pin: 'Support2025', rank: 'Support' }, // Example Support user
+    { username: 'admin-user', pin: 'Admin2025', rank: 'Admin' } // Example Admin user
 ];
 
 let currentUser = null;
@@ -23,6 +24,7 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
         updateHeader();
         showStartPage();
         checkSupportButtonVisibility();
+        checkAdminButtonVisibility();
     } else {
         alert('Benutzername oder PIN sind falsch!');
     }
@@ -51,7 +53,9 @@ function logout() {
     document.getElementById('login-container').classList.remove('hidden');
     document.getElementById('start-page').classList.add('hidden');
     document.getElementById('Support-page').classList.add('hidden');
+    document.getElementById('admin-page').classList.add('hidden');
     document.getElementById('support-button').classList.add('hidden');
+    document.getElementById('admin-button').classList.add('hidden');
 }
 
 // Funktion, um die Sichtbarkeit des Support-Buttons zu überprüfen
@@ -63,6 +67,15 @@ function checkSupportButtonVisibility() {
     }
 }
 
+// Funktion, um die Sichtbarkeit des Admin-Buttons zu überprüfen
+function checkAdminButtonVisibility() {
+    if (currentUser.rank === 'Admin' || currentUser.rank === 'Inhaber') {
+        document.getElementById('admin-button').classList.remove('hidden');
+    } else {
+        document.getElementById('admin-button').classList.add('hidden');
+    }
+}
+
 function showSupportPage() {
     document.getElementById('start-page').classList.add('hidden');
     document.getElementById('Support-page').classList.remove('hidden');
@@ -70,5 +83,15 @@ function showSupportPage() {
 
 function showStartPageFromSupport() {
     document.getElementById('Support-page').classList.add('hidden');
+    document.getElementById('start-page').classList.remove('hidden');
+}
+
+function showAdminPage() {
+    document.getElementById('start-page').classList.add('hidden');
+    document.getElementById('Admin-page').classList.remove('hidden');
+}
+
+function showStartPageFromAdmin() {
+    document.getElementById('Admin-page').classList.add('hidden');
     document.getElementById('start-page').classList.remove('hidden');
 }
