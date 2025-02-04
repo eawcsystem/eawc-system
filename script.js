0184c348bb6bbb8320eccc476a4a3ec66bf0e3ae
EAWC system <162746043+eawcsystem@users.noreply.github.com>
2025-02-04 20:49:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -93,9 +93,5 @@ function showStartPageFromAdmin() {
     document.getElementById('Moderator-page').classList.add('hidden');
     document.getElementById('start-page').classList.remove('hidden');
 }
-*/
 
-// Additional script to hide all visible elements
-document.addEventListener("DOMContentLoaded", function() {
-    document.body.style.display = "none";
 });
